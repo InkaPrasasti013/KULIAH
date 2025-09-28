b0e5e1a49e84dba116f9f3baa25a6d3b519c60d3
inka <inkaprasasti6@gmail.com>
2025-09-28 23:16:33 +0900

    Program Sistem Penilaian Mahasiswa dengan Conditional Statement

--- lib/tugas_mobile.dart
+++ lib/tugas_mobile.dart
@@ -1,3 +1,5 @@
-int calculate() {
-  return 6 * 7;
+import 'package:tugas_mobile/tugas_mobile.dart' as tugas_mobile;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_mobile.calculate()}!');
 }
